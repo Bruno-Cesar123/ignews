afaa8af333075445f08af004d4b4d9cf6c4d284b
Bruno Cesar <brunocesar12386@gmail.com>
2021-09-27 11:09:20 -0300

    config links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Pots</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Pots</a>
+          </ActiveLink>
         </nav>
         
         <SignInButton />
